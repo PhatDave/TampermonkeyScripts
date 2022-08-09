da1b7a7790a487f67372e610cd1a09ca4a54a5cb
M. David <david.majdandzic@inovatrend.com>
2022-08-09 13:15:51 +0200

    Update

--- SubredditBlocker.js
+++ SubredditBlocker.js
@@ -2,7 +2,7 @@
 // @name           Subreddit blocker
 // @author         Cyka
 // @include        *reddit.com/*
-// @version        1.0.0
+// @version        1.0
 // @run-at document-start
 // @updateURL   https://raw.githubusercontent.com/PhatDave/TampermonkeyScripts/master/SubredditBlocker.js
 // @downloadURL https://raw.githubusercontent.com/PhatDave/TampermonkeyScripts/master/SubredditBlocker.js
@@ -11,7 +11,7 @@
 // @noframes
 // ==/UserScript==
 
-let persistenceKey = "blockedReddits"
+let persistenceKey = "blockedReddits"a
 // GM_setValue(persistenceKey, JSON.stringify("[]"))
 let blocked = JSON.parse(await GM_getValue(persistenceKey, JSON.stringify("[]")));
 if (blocked.constructor === "".constructor) {
--- gitUpdate.js
+++ gitUpdate.js
@@ -1,4 +1,5 @@
 const exec = require('child_process').exec;
+const fs = require("fs");
 
 function execute(command, callback) {
 	exec(command, function(error, stdout, stderr) {
@@ -12,7 +13,34 @@ execute('git status', (out) => {
 		process.exit(0);
 	}
 
-	console.log(out);
+	// console.log(out);
+	out.split("\n").forEach((line) => {
+		if (/modified:\s*\w+\.js/.exec(line)) {
+			let modifiedFile = /modified:\s*(\w+\.js)/.exec(line)
+			modifiedFile = modifiedFile[1];
+			if (modifiedFile === "gitUpdate.js") {
+				return null;
+			}
+
+			let fileData = fs.readFileSync(modifiedFile, 'utf8');
+			let fileDataLines = fileData.split("\n");
+			for (let i = 0; i < fileDataLines.length; i++) {
+				let line = fileDataLines[i];
+				if (/@version\s*\d+\.\d+\.\d+/.exec(line)) {
+					let version = /(.+@version\s*\d+\.)(\d+)/.exec(line);
+					let minorVersion = version[2];
+					minorVersion++;
+					version = version[1] + minorVersion;
+					fileDataLines[i] = version;
+					`Updating version from ${line} to ${version}`;
+					break;
+				}
+			}
+			fileData = fileDataLines.join("\n");
+			// console.log(fileData);
+			fs.writeFileSync(modifiedFile, fileData);
+		}
+	});
 	if (out.includes("Changes not staged for commit") || out.includes("Untracked files")) {
 		execute('git add -A', (out) => {
 			console.log("Found modified files, doing commit...");
