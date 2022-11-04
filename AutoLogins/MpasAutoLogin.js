95f63230f98fd44ba31db6985a1b07bb8db7388e
David Majdandžić <david.majdandzic@inovatrend.com>
2022-11-04 09:10:49 +0100

    Update

--- AutoLogins/MpasAutoLogin.js
+++ AutoLogins/MpasAutoLogin.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         MPAS auto login
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        http://*/mpas/web/login*
