4b712a94a178c755f920c2cf8a2006446e81db63
David Majdandžić <david.majdandzic@inovatrend.com>
2022-11-04 09:09:51 +0100

    Update

--- AutoLogins/BssAutoLogin.js
+++ AutoLogins/BssAutoLogin.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BSS auto login
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        http://*/bss/web/loginpage*
