1322164a1192596305fde24fbcd20bcbd5e8f195
David Majdandžić <david.majdandzic@inovatrend.com>
2022-11-04 08:42:14 +0100

    Update

---
+++ AutoLogins/JiraAutoLogin.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         MPAS auto login
+// @version      0.2
+// @description  try to take over the world!
+// @author       You
+// @match        http://support.inovatrend.com/jira/login.jsp*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+let loginForm = document.querySelector(".form-body");
+
+let loginEntry = loginForm.querySelector("#login-form-username");
+let passwordEntry = loginForm.querySelector("#login-form-password");
+let loginButton = loginForm.querySelector("#login-form-submit");
+
+loginEntry.value = "david.majdandzic";
+passwordEntry.value = "-[dGFT6e65[@D4XPdJs_q<3(T4LM9K";
+loginButton.click();
