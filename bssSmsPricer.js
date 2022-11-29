8bf19264a9a7b74ec556a143d9a41f9c624bb018
David Majdandžić <david.majdandzic@inovatrend.com>
2022-11-29 13:15:48 +0100

    Auto commit

---
+++ bssSmsPricer.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name            BSS Poker
+// @version         0.5
+// @author          Cyka
+// @match           *://*/*
+// @run-at          document-end
+// @updateURL       https://raw.githubusercontent.com/PhatDave/TampermonkeyScripts/master/AutoLogin.js
+// @downloadURL     https://raw.githubusercontent.com/PhatDave/TampermonkeyScripts/master/AutoLogin.js
+// @grant           GM_getValue
+// @grant           GM_setValue
+// ==/UserScript==
+
+// let options = ['A*','a*','B*','b*','C*','c*','D*','d*','E*','e*','F*','f*','G*','g*','H*','h*','I*','i*','J*','j*','K*','k*','L*','l*','M*','m*','N*','n*','O*','o*','P*','p*','Q*','q*','R*','r*','S*','s*','T*','t*','U*','u*','V*','v*','W*','w*','X*','x*','Y*','y*','Z*','z*']
+let table = document.querySelector('table.table1');
+let input1 = table.querySelector('input#textfield');
+let input2 = table.querySelector('input#textfield_0');
+let input3 = table.querySelector('input#textfield_1');
+let submit = table.querySelector('input.button');
+
+let options = GM_getValue("options", []);
+let item = options.shift();
+GM_setValue("options", options);
+
+input1.value = item;
+input2.value = '0';
+submit.click();
