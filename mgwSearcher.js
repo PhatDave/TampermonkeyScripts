f6922a693128395141190f7b61e16b4d781ceee7
David Majdandžić <dmajdandzic@neos.hr>
2023-01-12 14:50:03 +0100

    Auto commit

--- mgwSearcher.js
+++ mgwSearcher.js
@@ -2,7 +2,7 @@
 // @name            MGW Searcher
 // @author          Cyka
 // @include         *search-requests/messages*
-// @version         1.0
+// @version         1.1
 // @run-at          document-idle
 // @updateURL       https://raw.githubusercontent.com/PhatDave/TampermonkeyScripts/master/mgwSearcher.js
 // @downloadURL     https://raw.githubusercontent.com/PhatDave/TampermonkeyScripts/master/mgwSearcher.js
@@ -43,32 +43,7 @@ function waitForElement(selector) {
 	});
 }
 
-waitForElement("input#rangePicker").then(() => {
-	let rangePicker = document.querySelector("input#rangePicker");
-	let date = new Date().getTime();
-
-	let dateFrom = new Date(date - (1000 * 60 * 5));
-	let dateTo = new Date(date + (1000 * 60 * 5));
-
-	let fromDay = padLeft(dateFrom.getDate(), 2, 0);
-	let fromMonth = padLeft(dateFrom.getMonth() + 1, 2, 0);
-	let fromYear = dateFrom.getFullYear();
-	let fromHour = padLeft(dateFrom.getHours(), 2, 0);
-	let fromMinute = padLeft(dateFrom.getMinutes(), 2, 0);
-	let fromSecond = padLeft(dateFrom.getSeconds(), 2, 0);
-
-	let toDay = padLeft(dateTo.getDate(), 2, 0);
-	let toMonth = padLeft(dateTo.getMonth() + 1, 2, 0);
-	let toYear = dateTo.getFullYear();
-	let toHour = padLeft(dateTo.getHours(), 2, 0);
-	let toMinute = padLeft(dateTo.getMinutes(), 2, 0);
-	let toSecond = padLeft(dateTo.getSeconds(), 2, 0);
-
-	let newValue = `${fromDay}-${fromMonth}-${fromYear} ${fromHour}:${fromMinute}:${fromSecond} - ${toDay}-${toMonth}-${toYear} ${toHour}:${toMinute}:${toSecond}`
-	rangePicker.value = newValue;
-	rangePicker.dispatchEvent(new Event("change"));
-	rangePicker.dispatchEvent(new Event("changed"));
-	rangePicker.dispatchEvent(new Event("update"));
-	rangePicker.dispatchEvent(new Event("updated"));
-	// Does not get picked up into request :(
+waitForElement(".range-button > button:nth-child(1)").then(() => {
+	let tenMinButton = document.querySelector(".range-button > button:nth-child(1)");
+	tenMinButton.click();
 });
