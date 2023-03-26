c0db140fbcf2fcb8b50f0593644c322522f187f2
David Majdandžić <dmajdandzic@neos.hr>
2023-03-26 18:52:02 +0200

    Add steamworkshoptojson

---
+++ SteamWorkshopToJson.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name            Steam App To RSS
+// @author          Cyka
+// @match           *steamcommunity.com/id/top_kek_id/myworkshopfiles*
+// @version         1.0
+// @run-at          document-end
+// @updateURL       https://raw.githubusercontent.com/PhatDave/TampermonkeyScripts/master/SteamWorkshopToJson.js
+// @downloadURL     https://raw.githubusercontent.com/PhatDave/TampermonkeyScripts/master/SteamWorkshopToJson.js
+// @noframes
+// ==/UserScript==
+
+function waitForElm(element, selector) {
+	return new Promise(resolve => {
+		if (element.querySelector(selector)) {
+			return resolve(element.querySelector(selector));
+		}
+
+		const observer = new MutationObserver(mutations => {
+			if (element.querySelector(selector)) {
+				resolve(element.querySelector(selector));
+				observer.disconnect();
+			}
+		});
+
+		observer.observe(document.body, {
+			childList: true,
+			subtree: true
+		});
+	});
+}
+
+function createElementFromHTML(htmlString) {
+	let div = document.createElement('div');
+	div.innerHTML = htmlString.trim();
+	return div.firstChild;
+}
+
+setTimeout(() => {
+	let allElements = JSON.parse(localStorage.getItem("cyka-steam-workshop-allElements")) || [];
+	let promises = [];
+
+	document.querySelectorAll(".workshopItemSubscription").forEach(element => {
+		if (element.id.includes("Subscription")) {
+			let promise = waitForElm(element, ".workshopItemSubscriptionDetails > a")
+			promises.push(promise);
+			promise.then(elm => {
+				let itemLink = element.querySelector(".workshopItemSubscriptionDetails > a").attributes.href.value;
+				allElements.push(itemLink);
+			});
+		}
+	});
+
+	Promise.all(promises).then(() => {
+		console.log(allElements);
+		localStorage.setItem("cyka-steam-workshop-allElements", JSON.stringify(allElements));
+		document.querySelectorAll(".pagebtn")[1].click();
+	});
+}, 250)
